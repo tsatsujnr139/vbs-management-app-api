d2c119f7029ccb4abec5ecc1d7a1fcd6478999eb
Tsatsu Adogla-Bessa Jnr <tsatsujnr@gmail.com>
2021-06-06 20:07:19 +0000

    updates

--- core/settings/base.py
+++ core/settings/base.py
@@ -154,8 +154,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_URL = '../static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = '..' / BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = "core.User"
